7f518ec5e7ed508465308af9cb924064ec0efc78
agricolab <robertsadresse@gmx.de>
2019-05-18 22:02:02 +0200

    Set pyglet audio options depending on platform

--- reiz/__init__.py
+++ reiz/__init__.py
@@ -1,5 +1,11 @@
 '''Root module for the pyreiz stimulus presentation toolbox
 '''
+import pyglet
+from sys import platform
+if platform == 'linux':
+    pyglet.options['audio'] = ('openal', 'pulse', 'directsound', 'silent')
+del pyglet, platform
+#%%
 import reiz.visual as visual
 import reiz.audio as audio
 import reiz.marker as marker
