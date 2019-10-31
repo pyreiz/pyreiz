e12ff89767275c1b6ed7cd2e2795a255b5e3d256
agricolab <robertsadresse@gmx.de>
2019-10-31 13:49:26 +0100

    Refactor audio

--- reiz/audio/__init__.py
+++ reiz/audio/__init__.py
@@ -1,20 +1,23 @@
 """
 API to auditory stimuli
 """
-import pyglet.media.player as _p
-Speaker = _p.Player
-from reiz.audio._primitives import AudioFile, Hertz
+from os import environ as _env
+from reiz.audio.primitives import AudioFile, Hertz
+
 
 def __get_path():
     import os
     LIBPATH = os.path.dirname(os.path.realpath(__file__))
-    LIBPATH = LIBPATH.split(os.path.sep + 'audio')[0]    
-    MEDIAPATH = os.path.join(LIBPATH, 'media')              
+    LIBPATH = LIBPATH.split(os.path.sep + 'audio')[0]
+    MEDIAPATH = os.path.join(LIBPATH, 'media')
     return os.path.join(MEDIAPATH, 'wav')
-  
+
+
 PATH = __get_path()
 
 # %%
+
+
 def make_library(path=None):
     'create an audio library from path'
     if path is None:
@@ -27,13 +30,15 @@ def make_library(path=None):
         key = key.replace("(", "")
         key = key.replace(")", "")
         key = key.replace(" ", "_")
-        key = key.replace("-", "_")         
+        key = key.replace("-", "_")
         key = key.strip()
-        val = AudioFile(os.path.join(path, f))        
+        val = AudioFile(os.path.join(path, f))
         library[key] = val
     library = SimpleNamespace(**library)
     return library
 
-from os import environ as _env 
-if not 'pyreiz-doc' in _env.keys():
+
+if not 'DOC' in _env.keys():
     library = make_library()
+else:
+    print("Generating sphinx documentation. Skipping audo library")
--- reiz/visual/__init__.py
+++ reiz/visual/__init__.py
@@ -1,15 +1,20 @@
+from os import environ as _env
 from reiz.visual._complex import Mural, Image, Cross, Circle, Background, Line, Bar
 from reiz.visual._complex import Polygon, Trapezoid, Cylinder
+
 
 def __get_path():
     import os
     LIBPATH = os.path.dirname(os.path.realpath(__file__))
-    LIBPATH = LIBPATH.split(os.path.sep + 'visual')[0]    
-    MEDIAPATH = os.path.join(LIBPATH, 'media')          
+    LIBPATH = LIBPATH.split(os.path.sep + 'visual')[0]
+    MEDIAPATH = os.path.join(LIBPATH, 'media')
     return os.path.join(MEDIAPATH, 'img')
-  
+
+
 PATH = __get_path()
 # %%
+
+
 def make_library(path=None):
     'create a library of visual stimuli from path'
     if path is None:
@@ -20,33 +25,36 @@ def make_library(path=None):
     for f in os.listdir(path):
         key, ext = os.path.splitext(f)
         if ext == '.ini':
-            import configparser, json
+            import configparser
+            import json
             ini = configparser.ConfigParser()
             ini.read(os.path.join(path, f))
-            sections = ini.sections();
+            sections = ini.sections()
             if 'Murals' in sections:
                 opts = ini.options('Murals')
-                for o  in opts:                
+                for o in opts:
                     val = ini.get('Murals', o)
                     library[o] = Mural(**json.loads(val))
-                
+
             if 'Cross' in sections:
                 opts = ini.options('Cross')
-                for o  in opts:                
+                for o in opts:
                     val = ini.get('Cross', o)
                     library[o] = Cross(**json.loads(val))
-   
+
         else:
             val = Image(os.path.join(path, f))
             key = key.replace("(", "")
             key = key.replace(")", "")
             key = key.replace(" ", "_")
-            key = key.replace("-", "_")         
+            key = key.replace("-", "_")
             key = key.strip()
         library[key] = val
     library = SimpleNamespace(**library)
     return library
 
-from os import environ as _env 
+
 if not 'DOC' in _env.keys():
     library = make_library()
+else:
+    print("Generating sphinx documentation. Skipping visual library")
